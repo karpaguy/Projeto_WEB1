2f416d569b38eebc1a99363c8df0ea3ef95daacf
karpaguy <denuzzidouglas@gmail.com>
2025-11-22 08:56:46 -0300

    gerarElementos Refatorados

--- assets/js/medicos.js
+++ assets/js/medicos.js
@@ -1,3 +1,5 @@
+
+
 function getMedicos() {
     return apiGET("medicos");
 }
@@ -14,15 +16,34 @@ function fecharModal() {
 }
 
 // ==================================
-//               CRUD
+//               AUXILIAR - ESP
 // ==================================
-async function carregarListaMedicos() {
+async function carregarEspecialidades() {
     const especialidades = await getEspecialidades();
 
-    const mapaEspecialidades = especialidades.reduce((acc, esp) => {
+    const mapa = especialidades.reduce((acc, esp) => {
         acc[esp.id] = esp.nome;
         return acc;
     }, {});
+
+    return mapa;
+}
+
+// Essa daqui é daora para caramba!
+// Ou lit 🔥🔥, como diriam os jovens.
+// tagHTML → a tag que deseja. Atributos → .textContent, .value, etc. ...children → quantidade indefinidade de 'filhos' que for passar pra tag HTML
+function geraElemento(tagHTML, atributos = {}, ...children) {
+    const elemento = document.createElement(tagHTML);
+    Object.assign(elemento, atributos);
+    children.forEach(c => elemento.append(c));
+    return elemento;
+}
+
+// ==================================
+//               CRUD
+// ==================================
+async function carregarListaMedicos() {
+    const mapaEspecialidades = await carregarEspecialidades();
 
     const medicosLista = await getMedicos();
     const tbody = document.createElement("tbody");
@@ -30,78 +51,52 @@ async function carregarListaMedicos() {
     medicosLista.forEach(medico => {
         const tr = document.createElement("tr");
 
-        const tdNome = document.createElement("td");
-        tdNome.textContent = medico.nome;
-
-        const tdCadastro = document.createElement("td");
-        tdCadastro.textContent = medico.dataCadastro;
-
-        const tdEspecialidade = document.createElement("td");
-        tdEspecialidade.textContent = mapaEspecialidades[medico.idEspecialidade] || "Desconhecida";
-
+        const tdNome = geraElemento("td", {textContent: medico.nome})
+        const tdCadastro = geraElemento("td", {textContent: medico.dataCadastro})
+        const tdEspecialidade = geraElemento("td", {textContent: mapaEspecialidades[medico.idEspecialidade] || "Desconhecida"})
         const tdAcoes = document.createElement("td");
         
         //============ BTN INFO =============
-        const btnInfo = document.createElement("button");
-        btnInfo.classList.add("info");
-        btnInfo.textContent = "👁 Ver consultas";
+        const btnInfo = geraElemento("button", {className: "info", textContent: "👁 Ver consultas"});
         btnInfo.addEventListener("click", async () => {
             console.log("A")
         })
 
         //============ BTN EDITAR =============
-        const btnEditar = document.createElement("button");
-        btnEditar.classList.add("warning");
-        btnEditar.textContent = "✏ Editar";
+        const btnEditar = geraElemento("button", {className: "warning", textContent: "✏ Editar"});
         btnEditar.addEventListener("click", async () => {
-            const div = document.createElement("div");
-            div.classList.add("modal_content");
+            const div = geraElemento("div", { className: "modal_content" });
+            const titulo = geraElemento("h2", { textContent: "Editar Médico" })
 
-            const titulo = document.createElement("h2");
-            titulo.textContent = "Editar Médico";
+            const labelNome = geraElemento("label", { textContent: "Nome:" });
+            const inputNome = geraElemento("input", {
+                type: "text",
+                value: medico.nome
+            });
 
-            const labelNome = document.createElement("label");
-            labelNome.textContent = "Nome:";
-
-            const inputNome = document.createElement("input");
-            inputNome.type = "text";
-            inputNome.value = medico.nome;
-
-            const selectEsp = document.createElement("select");
-            selectEsp.id = "select_esp";
-
-            const optionDefault = document.createElement("option");
-            optionDefault.value = "";
-            optionDefault.textContent = "Selecione...";
-            optionDefault.disabled = true;
-            selectEsp.appendChild(optionDefault);
+            const optionDefault = geraElemento("option", {
+                value: "",
+                textContent: "Selecione...",
+                disabled: true
+            });
+            const selectEsp = geraElemento("select", { id: "select_esp" }, optionDefault);
 
             const especialidades = await getEspecialidades();
             especialidades.forEach(esp => {
-                const option = document.createElement("option");
-                option.value = esp.id;
-                option.textContent = esp.nome;
-
-                if (esp.id === medico.idEspecialidade) {
-                    option.selected = true;
-                }
-
-                selectEsp.appendChild(option);
+                const option = geraElemento("option", {
+                    value: esp.id,
+                    textContent: esp.nome,
+                    selected: esp.id === medico.idEspecialidade
+                });
+                selectEsp.append(option);
             });
 
-            div.append(titulo);
-            div.append(labelNome);
-            div.append(inputNome);
+            const labelEsp = geraElemento("label", { textContent: "Especialidade:" });
 
-            const labelEsp = document.createElement("label");
-            labelEsp.textContent = "Especialidade:";
-            div.append(labelEsp);
-
-            div.append(selectEsp);
-
-            const btnSalvar = document.createElement("button");
-            btnSalvar.textContent = "Salvar";
-            btnSalvar.classList.add("info");
+            const btnSalvar = geraElemento("button", {
+                textContent: "Salvar",
+                className: "info"
+            });
 
             btnSalvar.addEventListener("click", async () => {
                 if (inputNome.value.trim() == "") {
@@ -119,15 +114,13 @@ async function carregarListaMedicos() {
                 fecharModal();
                 await carregarListaMedicos();
             });
-            div.append(btnSalvar);
+            div.append(titulo, labelNome, inputNome, labelEsp, selectEsp, btnSalvar);
 
             abrirModal(div);
         })
         
         //============ BTN DELETAR =============
-        const btnDeletar = document.createElement("button");
-        btnDeletar.classList.add("danger");
-        btnDeletar.textContent = "🗑 Deletar";
+        const btnDeletar = geraElemento("button", {className: "danger", textContent: "🗑 Deletar"});
         btnDeletar.addEventListener("click", async () => {
             if (confirm(`Tem certeza que deseja excluir o medico ${medico.nome}?`)) {
                 await apiDELETE("medicos", medico.id);
@@ -135,90 +128,37 @@ async function carregarListaMedicos() {
             }
         })
 
-        tdAcoes.appendChild(btnInfo);
-        tdAcoes.appendChild(btnEditar);
-        tdAcoes.appendChild(btnDeletar);
-
-        tr.appendChild(tdNome);
-        tr.appendChild(tdCadastro);
-        tr.appendChild(tdEspecialidade);
-        tr.appendChild(tdAcoes);
-
+        tdAcoes.appendChild(btnInfo, btnEditar, btnDeletar);
+        tr.appendChild(tdNome, tdCadastro, tdEspecialidade, tdAcoes);
         tbody.appendChild(tr);
     })
     return tbody;
 }
 
 async function carregarCadastroMedicos() {
-    const container = document.createElement("div");
-    container.className = "cadastro_container";
-
-    const title = document.createElement("h1");
-    title.textContent = "Cadastrar novo médico";
-    container.appendChild(title);
+    const title = geraElemento("h2", {textContent: "Cadastrar novo médico"});
+    const container = geraElemento("div", {className: "cadastro_contaienr"}, title);
 
     // === NOME ===
-    const grupoNome = document.createElement("div");
-    grupoNome.className = "form_group";
-
-    const labelNome = document.createElement("label");
-    labelNome.textContent = "Nome";
-    labelNome.setAttribute("for", "input_nome");
-
-    const inputNome = document.createElement("input");
-    inputNome.id = "input_nome";
-    inputNome.type = "text";
-    inputNome.placeholder = "Nome";
-
-    const erroNome = document.createElement("span");
-    erroNome.className = "erro";
-    erroNome.textContent = "Nome inválido";
-    erroNome.style.display = "none";
-
-    grupoNome.appendChild(labelNome);
-    grupoNome.appendChild(inputNome);
-    grupoNome.appendChild(erroNome);
+    const labelNome = geraElemento("label", {textContent: "Nome", htmlFor: "input_nome"});
+    const inputNome = geraElemento("input", {id: "input_nome", type: "text", placeholder: "Nome"});
+    const grupoNome = geraElemento("div", {className: "form_group"}, labelNome, inputNome);
 
     // === ESPECIALIDADE ===
-    const grupoEsp = document.createElement("div");
-    grupoEsp.className = "form_group";
-
-    const labelEsp = document.createElement("label");
-    labelEsp.textContent = "Especialidade";
-    labelEsp.setAttribute("for", "select_esp");
-
-    const selectEsp = document.createElement("select");
-    selectEsp.id = "select_esp";
-
-    const optionDefault = document.createElement("option");
-    optionDefault.value = "";
-    optionDefault.textContent = "Selecione...";
-    optionDefault.disabled = true;
-    optionDefault.selected = true;
-    selectEsp.appendChild(optionDefault);
+    const labelEsp = geraElemento("label", {textContent: "Especialidade", htmlFor: "select_esp"});
+    
+    const optionDefault = geraElemento("option", {value: "", textContent: "Selecione...", disabled: true, selected: true})
+    const selectEsp = geraElemento("select", {id: "select_esp"}, optionDefault);
 
     const especialidades = await getEspecialidades();
-        especialidades.forEach(esp => {
-        const option = document.createElement("option");
-        option.value = esp.id;
-        option.textContent = esp.nome;
+    especialidades.forEach(esp => {
+        const option = document.createElement("option"); geraElemento("option", {value: esp.id, textContent: esp.nome})
         selectEsp.appendChild(option);
     });
-
-    const erroEsp = document.createElement("span");
-    erroEsp.className = "danger";
-    erroEsp.textContent = "Especialização";
-    erroEsp.style.display = "none";
-
-    grupoEsp.appendChild(labelEsp);
-    grupoEsp.appendChild(selectEsp);
-    grupoEsp.appendChild(erroEsp);
+    const grupoEsp = geraElemento("div", {className: "form_group"}, labelEsp, selectEsp);
 
     // === BOTÃO ===
-    const btnCadastrar = document.createElement("button");
-    btnCadastrar.className = "btn_cadastrar";
-    btnCadastrar.textContent = "Cadastrar";
-
+    const btnCadastrar = geraElemento("button", {className: "btn_cadastrar", textContent: "Cadastrar"})
     btnCadastrar.addEventListener("click", () => {
         const nomeMedico = inputNome.value;
         // .trim() vai remover espaços, mas não aqueles que estiverem entre algo escrito.
@@ -239,7 +179,6 @@ async function carregarCadastroMedicos() {
     container.appendChild(grupoNome);
     container.appendChild(grupoEsp);
     container.appendChild(btnCadastrar);
-
     return container;
 }
 
@@ -248,7 +187,7 @@ async function carregarCadastroMedicos() {
 // ==================================
 function abrirModal(conteudoElemento) {
     const modal = document.getElementById("modal_container");
-    const content = document.getElementById("modal_content");
+    const content = gerarElemento("modal_content", {innerHTML: ""},);
 
     content.innerHTML = "";
     content.appendChild(conteudoElemento);
